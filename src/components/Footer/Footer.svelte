0871500c10c9c5939c886c7bfee069182e1593a4
Weston Vincze <westonvincze@gmail.com>
2024-10-04 17:16:28 -0400

    ✨ Add Footer with Tailwind and Flowbite integration

--- src/components/Footer/Footer.svelte
+++ src/components/Footer/Footer.svelte
@@ -1,11 +1,47 @@
 <script lang="ts">
-
+  import { FooterBrand, FooterIcon, Footer, FooterCopyright, FooterLinkGroup, FooterLink } from 'flowbite-svelte';
+  import { FacebookSolid, InstagramSolid } from 'flowbite-svelte-icons';
 </script>
 
-<footer>
-
-</footer>
-
-<style>
-
-</style>
+<Footer class="dark:bg-gray-500" footerType="socialmedia">
+  <div class="md:flex md:justify-between">
+    <div class="mb-6 md:mb-0">
+      <FooterBrand href="/" src="Logo.svg" alt="Grand Tour BBQ Logo" name="Grand Tour BBQ" />
+    </div>
+    <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+      <div>
+        <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
+        <FooterLinkGroup>
+          <FooterLink liClass="mb-4" href="/">Flowbite</FooterLink>
+          <FooterLink liClass="mb-4" href="/">Tailwind CSS</FooterLink>
+        </FooterLinkGroup>
+      </div>
+      <div>
+        <h2 class="mb-6 text-sm font-semibold uppercase text-gray-900 dark:text-white">Follow us</h2>
+        <FooterLinkGroup>
+          <FooterLink liClass="mb-4" href="/">GitHub</FooterLink>
+          <FooterLink liClass="mb-4" href="/">Discord</FooterLink>
+        </FooterLinkGroup>
+      </div>
+      <div>
+        <h2 class="mb-6 text-sm font-semibold uppercase text-gray-900 dark:text-white">Legal</h2>
+        <FooterLinkGroup>
+          <FooterLink liClass="mb-4" href="/">Privacy Policy</FooterLink>
+          <FooterLink liClass="mb-4" href="/">Terms & Conditions</FooterLink>
+        </FooterLinkGroup>
+      </div>
+    </div>
+  </div>
+  <hr class="my-6 border-gray-200 sm:mx-auto dark:border-primary-500 lg:my-8" />
+  <div class="sm:flex sm:items-center sm:justify-between">
+    <FooterCopyright href="/" by="Copyright © 2024 Grand Tour BBQ Inc - All Rights Reserved" />
+    <div class="flex mt-4 space-x-6 rtl:space-x-reverse sm:justify-center sm:mt-0">
+      <FooterIcon href="/">
+        <FacebookSolid class="w-5 h-5 text-gray-900 dark:text-white hover:text-gray-900 dark:hover:text-white" />
+      </FooterIcon>
+      <FooterIcon href="/">
+        <InstagramSolid class="w-5 h-5 text-gray-900 dark:text-white hover:text-gray-900 dark:hover:text-white" />
+      </FooterIcon>
+    </div>
+  </div>
+</Footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,12 @@
-<script lang="ts">
-	import { Footer } from "../components/Footer";
-  import { Header } from "../components/Header";
-  import "../styles/global.css";
+<script>
+	import { Footer } from '../components/Footer';
+	import { Header } from '../components/Header';
+	import '../styles/global.css';
+	import '../app.css';
 </script>
 
 <Header />
 
-<slot />
+<slot></slot>
 
 <Footer />
-
