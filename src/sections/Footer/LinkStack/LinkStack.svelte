15e9676944edf604f2747b034af4319b2b4adafd
Weston Vincze <westonvincze@gmail.com>
2024-10-29 23:12:50 -0400

    ✨ Add LinkStack for use in footer

---
+++ src/sections/Footer/LinkStack/LinkStack.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  type Link = {
+    text: string;
+    url: string;
+  }
+  export let heading: string;
+  export let links: Link[];
+
+</script>
+
+<div>
+  <h3>{heading}</h3>
+
+  <div class="links">
+    {#each links as link}
+      <a href="{link.url}">{link.text}</a>
+    {/each}
+  </div>
+</div>
+
+<style>
+  h3 {
+    text-transform: uppercase;
+    font-weight: 800;
+  }
+  .links {
+    display: flex;
+    flex-direction: column;
+    font-size: var(--font-size-sm);
+    font-weight: 300;
+  }
+</style>
---
+++ src/sections/Footer/LinkStack/index.ts
@@ -0,0 +1,1 @@
+export { default as LinkStack } from "./LinkStack.svelte";
