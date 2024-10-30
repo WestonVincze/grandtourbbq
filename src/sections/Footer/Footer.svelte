1ee9bb9d2d4de69450e548d91e5df8248fd3967c
Weston Vincze <westonvincze@gmail.com>
2024-10-29 23:11:40 -0400

    ♻️ Refactor footer

--- src/sections/Footer/Footer.svelte
+++ src/sections/Footer/Footer.svelte
@@ -1,47 +1,92 @@
 <script lang="ts">
-  import { FooterBrand, FooterIcon, Footer, FooterCopyright, FooterLinkGroup, FooterLink } from 'flowbite-svelte';
-  import { FacebookSolid, InstagramSolid } from 'flowbite-svelte-icons';
+  import Logo from "$icons/Logo.svg";
+	import { SocialLinks } from '$components';
+	import { LinkStack } from './LinkStack';
 </script>
 
-<Footer class="dark:bg-gray-500" footerType="socialmedia">
-  <div class="md:flex md:justify-between">
-    <div class="mb-6 md:mb-0">
-      <FooterBrand href="/" src="Logo.svg" alt="Grand Tour BBQ Logo" name="Grand Tour BBQ" />
+<footer>
+  <div class="container">
+    <div class="section upper">
+      <div class="brand">
+        <img class="logo" src={Logo} alt="Grand Tour BBQ Logo" />
+      </div>
+
+      <div class="links">
+        <LinkStack heading="Menus" links={[
+            { url: "/catering", text: "Catering" },
+            { url: "/restaurant", text: "Restaurant" },
+            { url: "/food-truck", text: "Food Truck" },
+          ]}
+        />
+        <LinkStack heading="Quick Links" links={[
+            { url: "#", text: "Catering Services" },
+            { url: "#", text: "Event Schedule" },
+          ]}
+        />
+        <LinkStack heading="Contact Info" links={[
+            { url: "#", text: "Email Us" },
+            { url: "#", text: "Call Us" },
+          ]}
+        />
+        <LinkStack heading="Follow Us" links={[
+            { url: "#", text: "Facebook Page" },
+            { url: "#", text: "Instagram Feed" },
+          ]}
+        />
+        <LinkStack heading="Resources" links={[
+            { url: "#", text: "Blog" },
+            { url: "#", text: "Reviews" },
+            { url: "#", text: "FAQs" },
+          ]}
+        />
+        <LinkStack heading="Legal" links={[
+            { url: "#", text: "Terms & Conditions" },
+            { url: "#", text: "Privacy Policy" },
+          ]}
+        />
+      </div>
     </div>
-    <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
-      <div>
-        <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
-        <FooterLinkGroup>
-          <FooterLink liClass="mb-4" href="/">Flowbite</FooterLink>
-          <FooterLink liClass="mb-4" href="/">Tailwind CSS</FooterLink>
-        </FooterLinkGroup>
-      </div>
-      <div>
-        <h2 class="mb-6 text-sm font-semibold uppercase text-gray-900 dark:text-white">Follow us</h2>
-        <FooterLinkGroup>
-          <FooterLink liClass="mb-4" href="/">GitHub</FooterLink>
-          <FooterLink liClass="mb-4" href="/">Discord</FooterLink>
-        </FooterLinkGroup>
-      </div>
-      <div>
-        <h2 class="mb-6 text-sm font-semibold uppercase text-gray-900 dark:text-white">Legal</h2>
-        <FooterLinkGroup>
-          <FooterLink liClass="mb-4" href="/">Privacy Policy</FooterLink>
-          <FooterLink liClass="mb-4" href="/">Terms & Conditions</FooterLink>
-        </FooterLinkGroup>
-      </div>
+
+    <div class="section lower">
+      <span class="copyright">
+        {'Copyright © 2024 Grand Tour BBQ Inc - All Rights Reserved'}
+      </span>
+      <span class="hst">
+        {'HST# 726244411RT0001'}
+      </span>
+      <SocialLinks />
     </div>
   </div>
-  <hr class="my-6 border-gray-200 sm:mx-auto dark:border-primary-500 lg:my-8" />
-  <div class="sm:flex sm:items-center sm:justify-between">
-    <FooterCopyright href="/" by="Copyright © 2024 Grand Tour BBQ Inc - All Rights Reserved" />
-    <div class="flex mt-4 space-x-6 rtl:space-x-reverse sm:justify-center sm:mt-0">
-      <FooterIcon href="/">
-        <FacebookSolid class="w-5 h-5 text-gray-900 dark:text-white hover:text-gray-900 dark:hover:text-white" />
-      </FooterIcon>
-      <FooterIcon href="/">
-        <InstagramSolid class="w-5 h-5 text-gray-900 dark:text-white hover:text-gray-900 dark:hover:text-white" />
-      </FooterIcon>
-    </div>
-  </div>
-</Footer>
+
+</footer>
+
+<style>
+  footer {
+    background-color: var(--black);
+    padding: var(--size-2) 0;
+  }
+  .section {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: var(--size-2);
+    padding: var(--size-2) 0;
+  }
+  .upper {
+    border-bottom: 1px solid var(--accent);
+  }
+  .links {
+    display: flex;
+    gap: var(--size-3);
+  }
+  .logo {
+    width: 100px;
+  }
+  .copyright {
+    flex-grow: 1;
+  }
+  .copyright, .hst {
+    color: var(--gray-light);
+    font-size: var(--font-size-sm);
+  }
+</style>
